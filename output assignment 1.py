50ee7740dd26a96e08dd8af238f0c571e23eee10
gungun3018 <97682454+gungun3018@users.noreply.github.com>
2022-01-14 11:04:18 +0530

    Add files via upload

---
+++ output assignment 1.py
@@ -0,0 +1,42 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:\Users\HP\Desktop\assigment 1.py =================
+ANSWER-1 
+
+ Enter the 1st number: 50
+ Enter the 2nd number: 30
+ Enter the 3rd number: 100
+The average of the 3 numbers is 113.33333333333334 
+
+ANSWER-2
+Enter the gross income to the nearest penny:45000
+Enter the number of dependents: 6
+TAX comes out to be: 3400.0 
+
+ANSWER-3 
+
+SID: 21105023
+Name: Gungun
+Gender: F
+CGPA:9.99
+Course Name: Electronics and Communication Engineering
+ STUDENT GENERAL INFORMATION...
+ANSWER-4 
+
+Enter the marks of Student 1:98
+Enter the marks of Student 2:78
+Enter the marks of Student 3:85
+Enter the marks of Student 4:65
+Enter the marks of Student 5:92
+The marks of the 5 students are as follows: [98, 78, 85, 65, 92]
+Sorted marks of 5 student: [65, 78, 85, 92, 98] 
+
+ANSWER-5 
+
+The list of colours is: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+The modified list of colours is: ['Red', 'Green', 'White', 'Black', 'Yellow'] 
+
+The required list is: ['Red', 'Green', 'White', 'Black', 'Purple'] 
+
