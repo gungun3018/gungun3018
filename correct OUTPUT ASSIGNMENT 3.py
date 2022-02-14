a50b5cbefb03939ffe0dc5ea457e2e8a9ea61c63
gungun3018 <97682454+gungun3018@users.noreply.github.com>
2022-02-14 15:40:15 +0530

    Add files via upload

---
+++ correct OUTPUT ASSIGNMENT 3.py
@@ -0,0 +1,75 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/HP/Desktop/ASSIGNMENT 3.py =================
+SOLUTION TO QUESTION 1
+Solution 1
+Enter the string: My name is Gungun
+{'MY': 1, 'NAME': 1, 'IS': 1, 'GUNGUN': 1} 
+
+SOLUTION TO QUESTION 2
+Please enter Day- 30
+Please enter Month- 11
+Please enter Year- 2021
+Next Date is: 1/12/2021 
+
+SOLUTION TO QUESTION 3 
+
+Enter a number 1 : 5
+Enter a number 2 : 6
+Enter a number 3 : 7
+The required list of tuples with the second element as the square of the first element is:  [(5, 25), (6, 36), (7, 49)] 
+
+SOLUTION TO QUESTION 4 
+
+Enter your grade point: 10
+Your grade is   A+   and   Outstanding 
+
+SOLUTION TO QUESTION 5 
+
+ABCDEFGHIGK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+SOLUTION TO QUESTION 6 
+
+Enter the name: Gungun
+Enter the SID: 21105023
+If you want to feed more details type "Y" else "N": Y
+Students details are as follows: (SID, Student name) 
+[(21105023, 'Gungun')]
+
+Sorted dictionary by student name:[(21105023, 'Gungun')]
+
+Sorted dictionary by SID: [(21105023, 'Gungun')]
+
+Enter the SID whose name you want to know: 21105023
+The name of the student with SID 21105023 is :Gungun
+
+21105023 is Gungun
+value sorted dictionary is ['Gungun']
+Keys sorted dictionary is [21105023]
+Please enter the student's SID whose detail you need-  21105023
+Name of the required student is Gungun
+SOLUTION TO QUESTION 7 
+
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 10
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of total 10 terms of sequence is:8.8 
+SOLUTION TO QUESTION 8 
+
+Set of all elements that are in Set1 and Set2 but not both: {2, 4}
+Set of elements that is only present in one set is: {17, 3, 6, 8, 9, 13}
+Set of elements that is only present in two set is: {1, 2, 4, 5}
+set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
