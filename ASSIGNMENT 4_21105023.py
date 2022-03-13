866ffd1ec0680daeb9c0cf68cd7a609542158ad2
gungun3018 <97682454+gungun3018@users.noreply.github.com>
2022-03-13 17:34:46 +0530

    Update ASSIGNMENT 4_21105023.py

--- ASSIGNMENT 4_21105023.py
+++ ASSIGNMENT 4_21105023.py
@@ -40,18 +40,6 @@ for i in range(n):
     print()
 
     
-#method 2
-
-n = int(input("Enter the number of rows:"))  
-  
-# iterarte upto n  
-for i in range(n):  
-   # adjust space  
-   print(' '*(n-i), end='')  
-  
-   # compute power of 11  
-   print(' '.join(map(str, str(11**i))))  
-
 
 #QUESTION 3
 print("SOLUTION TO QUESTION 3 ","\n")
